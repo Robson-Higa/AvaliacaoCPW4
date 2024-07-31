b4f3aa4471a2f74532ab7847d667897c4f08972a
unknown <Robson Higa>
2024-07-31 15:54:34 -0400

    atualizado

--- colorpanel/cypress/e2e/app.cy.ts
+++ colorpanel/cypress/e2e/app.cy.ts
@@ -1,20 +1,15 @@
 describe("e2e tests", () => {
   beforeEach(() => {
     cy.visit("");
-    cy.fixture("values.json").as("values");
+    //cy.fixture("values.json").as("values");
   });
+   it('deve ajustar a cor exibida quando os inputs de cor são modificados', function () {
+    cy.get(':nth-child(1) > input').invoke('val', '100').trigger('change');
+    cy.get(':nth-child(2) > input').invoke('val', '15').trigger('change');
+    cy.get(':nth-child(3) > input').invoke('val', '20').trigger('change');
+    cy.get(':nth-child(4) > input').invoke('val', '0.8').trigger('input');
+    cy.wait(1000); 
+    cy.get('.styles_panel__Dze9I').should('have.css', 'background-color', `rgba(100, 15, 20, 0.8)`);
 
-  
-
-  it("should decrement counter correctly", function () {
-    const sentence = this.values.sentence;
-    cy.get('[data-cy="sentence-input"]').type(sentence);
-
-    for (let i = 0; i < sentence.length; i++) {
-      cy.get('[data-cy="sentence-input"]').type("{backspace}");
-    }
-
-    const result = `A frase contém 0 caracteres.`;
-    cy.get('[data-cy="counter-result"]').should("have.text", result);
-  });
+  })
 });
