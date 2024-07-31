a9c0070060fb424c3c39a27dfa9ee25c4e922da3
unknown <Robson Higa>
2024-07-31 15:58:02 -0400

    atualizado

--- colorpanel/cypress/e2e/app.cy.ts
+++ colorpanel/cypress/e2e/app.cy.ts
@@ -3,7 +3,7 @@ describe("e2e tests", () => {
     cy.visit("");
     //cy.fixture("values.json").as("values");
   });
-   it('deve ajustar a cor exibida quando os inputs de cor são modificados', function () {
+   it('should show a color correctly', function () {
     cy.get(':nth-child(1) > input').invoke('val', '100').trigger('change');
     cy.get(':nth-child(2) > input').invoke('val', '15').trigger('change');
     cy.get(':nth-child(3) > input').invoke('val', '20').trigger('change');
